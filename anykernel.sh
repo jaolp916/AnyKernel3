d676ffa2e3cd46e639623d6faca1a0abd48d7cdf
jaolp916 <3192396256@qq.com>
2025-09-25 17:19:47 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=GKI kernel by Jianke
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -13,8 +13,6 @@ do.cleanuponabort=0
 device.name1=
 device.name2=
 device.name3=
-device.name4=
-device.name5=
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -32,17 +30,6 @@ no_magisk_check=1
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
 
-kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
-case $kernel_version in
-    5.1*) ksu_supported=true ;;
-    6.1*) ksu_supported=true ;;
-    6.6*) ksu_supported=true ;;
-    *) ksu_supported=false ;;
-esac
-
-ui_print " " "  -> ksu_supported: $ksu_supported"
-$ksu_supported || abort "  -> Non-GKI device, abort."
-
 # boot install
 split_boot
 if [ -f "split_img/ramdisk.cpio" ]; then
@@ -52,3 +39,7 @@ else
     flash_boot
 fi
 ## end boot install
+
+ui_print "   5.10 GKI Sukisu    ";
+ui_print "   Kernel by Jianke   ";
+
